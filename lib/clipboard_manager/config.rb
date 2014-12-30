0c7fe06fe97dbaf54b2e238f85b559cf6be901a2
carlosjhr64 <carlosjhr64@gmail.com>
2014-12-30 13:42:04 -0800

    only want major.minor on About dialog

--- lib/clipboard_manager/config.rb
+++ lib/clipboard_manager/config.rb
@@ -1,4 +1,5 @@
 module ClipboardManager
+  using Rafini::String
 
   help = <<-HELP
 Usage: gtk3app clipboardmanager [options]
@@ -55,7 +56,7 @@ use no-ask and no-running for false.
 
       about_dialog: {
         set_program_name: 'Clipboard Manager',
-        set_version: VERSION,
+        set_version: VERSION.semantic(0..1),
         set_copyright: '(c) 2014 CarlosJHR64',
         set_comments: 'A Ruby Gtk3App Clipboard Manager ',
         set_website: 'https://github.com/carlosjhr64/clipboard_manager',
